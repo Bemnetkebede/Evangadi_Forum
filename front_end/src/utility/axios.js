bbbb46f04ae7eedfaef596df33d02dd7ee56437a
Bemnet Kebede <bemnet026@gmail.com>
2025-01-04 04:30:47 +0300

    done

--- front_end/src/utility/axios.js
+++ front_end/src/utility/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL, 
-})
+    baseURL: import.meta.env.REACT_APP_API_URL,
+});
+
 export default axiosInstance;
+
 
     // baseURL: 'http://localhost:3306/api', 
